1a4f99bc0de86eb32b6f5085c60e39295bd9b645
mateusoliiveira <teusolive@gmail.com>
2022-08-24 04:53:29 -0300

    update: follow

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -22,19 +22,19 @@ class PublicationController extends Controller
     public function index()
     {
        //return $this->model->get();
-      return DB::table("publications")
-      ->leftJoin("users", function($join){
-        $join->on("publications.user_id", "=", "users.id");
-      })
-      ->select("users.id", "publications.id", "publications.body", "publications.created_at")
-      ->where("publications.user_id", "=", $this->request->authedUser()['id'])
-      ->whereIn("publications.user_id", function($query){
-        $query->from("users_followings")
-        ->select("user_followed")
-        ->where("user_id", "=", $this->request->authedUser()['id']);
-      })
-      ->orderBy("publications.created_at","desc")
-      ->get();
+    return DB::table("publications")
+    ->leftJoin("users", function($join){
+      $join->on("publications.user_id", "=", "users.id");
+    })
+    ->select("users.id", "publications.id", "publications.body", "publications.created_at")
+    ->where("publications.user_id", "=", $this->request->authedUser()['id'])
+    ->whereIn("publications.user_id", function($query){
+      $query->from("follows")
+      ->select("user_followed_id")
+      ->where("user_id", "=", $this->request->authedUser()['id']);
+    })
+    ->orderBy("publications.created_at","desc")
+    ->get();
     }
 
     // public function indexWith()
